7f9f8ef88046a80b9b2e6f4333311707a953e78b
Keenan <arcknine@skolpad.org>
2012-10-09 00:00:37 +0800

    Splash page is now in CI

--- application/views/splash.php
+++ application/views/splash.php
@@ -3,11 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Shop With Internet</title>
-<link rel="stylesheet" type="text/css" href="styles.css" />
+<link rel="stylesheet" type="text/css" href="css/splash-style.css" />
+<script type="text/javascript" src="js/main.js"></script>
 </head>
 <body>
 <div style="margin:10% 0 0 0; text-align:center;">
-	<img src="coming soon.png" width="408" height="168" />
+	<img src="images/coming soon.png" width="408" height="168" />
 	<p><a href="http://www.shopwith.net/">ShopWith.Net</a> is a social E-commerce with easy 1 2 3 steps to create<br />your online business website for your products and services.</p>
 </div>
 </body>
