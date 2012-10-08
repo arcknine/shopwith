8a312ec6cad899c071ef5b7917ca083a54305933
Keenan <arcknine@skolpad.org>
2012-10-09 00:32:02 +0800

    Added layout lib

--- application/controllers/splash.php
+++ application/controllers/splash.php
@@ -19,7 +19,8 @@ class Splash extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('splash');
+		$this->layout->layout('layouts/splash');
+		$this->layout->view('splash-view');
 	}
 }
 
---
+++ application/views/splash-view.php
@@ -0,0 +1,4 @@
+<div style="margin:10% 0 0 0; text-align:center;">
+  <img src="images/coming soon.png" width="408" height="168" />
+  <p><a href="http://www.shopwith.net/">ShopWith.Net</a> is a social E-commerce with easy 1 2 3 steps to create<br />your online business website for your products and services.</p>
+</div>
