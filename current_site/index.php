e788dc9e26f63c5ce193bc7dbf51e30219f4f557
Keenan <arcknine@skolpad.org>
2012-10-08 23:37:14 +0800

    Created shopwith.net repo

---
+++ current_site/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Shop With Internet</title>
+<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+<div style="margin:10% 0 0 0; text-align:center;">
+	<img src="coming soon.png" width="408" height="168" />
+	<p><a href="http://www.shopwith.net/">ShopWith.Net</a> is a social E-commerce with easy 1 2 3 steps to create<br />your online business website for your products and services.</p>
+</div>
+</body>
+</html>
