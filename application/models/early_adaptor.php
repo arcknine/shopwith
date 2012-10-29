9390d7e78ad508f571c6c3223bb5e30c1c17e896
Keenan Iban <arcknine@skolpad.org>
2012-10-29 23:08:42 +0800

    Modified some script on early_adaptor model

--- application/models/early_adaptor.php
+++ application/models/early_adaptor.php
@@ -19,11 +19,14 @@ class Early_adaptor extends CI_Model {
 		{
 			$this->load->library('email');
 			
+			$config['mailtype'] = 'html';
+			$this->email->initialize($config);
+			
 			$this->email->from('no-reply@shopwith.net', 'Shopwith.net');
 			$this->email->to('keenan@smartstartupidea.com, reynaldo@smartstartupidea.com'); 
 			$this->email->subject('[Shopwith.net] Sign-up');
 			$msg = $this->load->view('email/early-adaptor', $data, true);
-			$this->email->message('Testing the email class.');	
+			$this->email->message($msg);	
 			$this->email->send();
 		}
 	}
